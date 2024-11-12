746e8ce8f9dde61e72f9ada3c55ebe370d2c2489
unknown <srikanth8605@gmail.com>
2024-11-12 16:26:32 +0530

    mailer code

--- contact.php
+++ contact.php
@@ -160,3 +160,37 @@
     <?php include 'includes/footer.php'; ?>
 
 </html>
+
+
+<?php 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form fields
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $message = trim($_POST["message"]);
+
+    // Set the recipient email address.
+    $to = "info@asstudiovision.com";
+
+    // Set the email subject.
+    $subject = "New Contact Form Submission from $name";
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $headers = "From: $email";
+
+    // Send the email
+    if (mail($to, $subject, $email_content, $headers)) {
+        // Redirect to a thank you page or display a success message
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+} else {
+    echo "There was a problem with your submission, please try again.";
+}
+?>
